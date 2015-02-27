6c7e7e8a3db5822e7f0b7b6aede2655e25859b0c
Henry Rao <jaohaohsuan@gmail.com>
2015-02-27 15:52:05 +0800

    adding more tests regularly

--- src/main/scala/com/example/Demo1Service.scala
+++ src/main/scala/com/example/Demo1Service.scala
@@ -1,8 +1,10 @@
 package com.example
 
 import spray.http._
+import spray.routing
 import spray.routing._
 import MediaTypes._
+import StatusCodes._
 
 trait Demo1Service extends HttpService {
 
@@ -18,6 +20,43 @@ trait Demo1Service extends HttpService {
         }
       }
     }
+  } ~ path("foo" ~ Slash.?) {
+    get {
+      complete("/foo")
+    }
+  } ~ path("i" ~ IntNumber) { i =>
+    get {
+      complete(s"IntNumber:$i")
+    }
+  } ~ path("color" / Map("red" -> 1, "green" -> 2)) { value =>
+    get {
+      complete(s"$value")
+    }
+  } ~ path("profile" / "id" ~ Segment) { id =>
+    complete(s"$id")
+  } ~ pathPrefix("unmatched"){
+    unmatchedPath { u =>
+      complete(s"$u")
+    }
+  } ~ pathPrefix("pathEnd"){
+        pathEnd {
+          complete("/pathEnd")
+        } ~
+        path("continue"){
+          complete("continue")
+        }
+
+  }
+
+  val handledRoute = pathPrefix("handled") {
+    handleRejections(RejectionHandler {
+      case Nil => complete(NotFound,"Oh man, what you are looking for is long gone.")
+    }) {
+        path("existing"){
+          complete("This path exists")
+        }
+    }
+
   }
 
 }
--- src/test/scala/com/example/Demo1ServiceSpec.scala
+++ src/test/scala/com/example/Demo1ServiceSpec.scala
@@ -29,6 +29,64 @@ class Demo1ServiceSpec extends Specification with Specs2RouteTest with Demo1Serv
       }
     }
 
+    "return a path '/foo' or '/foo/'" in {
+      Get("/foo") ~> demo1Route ~> check {
+        responseAs[String] === "/foo"
+      }
+      Get("/foo/") ~> demo1Route ~> check {
+        responseAs[String] === "/foo"
+      }
+    }
+
+    "return an IntNumber:50" in {
+      Get("/i50") ~> demo1Route ~> check {
+        responseAs[String] === "IntNumber:50"
+      }
+    }
+
+    "matches '/color/red' or '/color/green' and extracts 1 or 2 respectively" in {
+      Get("/color/red") ~> demo1Route ~> check {
+        responseAs[String] === "1"
+      }
+      Get("/color/green") ~> demo1Route ~> check {
+        responseAs[String] === "2"
+      }
+    }
+
+    "matches '/profile/id1234' and extracts 1234" in {
+      Get("/profile/id1234") ~> demo1Route ~> check {
+        responseAs[String] === "1234"
+      }
+    }
+
+    "matches '/unmatched/wtf...' and extracts /wtf..." in {
+      Get("/unmatched/wtf...") ~> demo1Route ~> check {
+        responseAs[String] === "/wtf..."
+      }
+    }
+
+    "path '/pathEnd' already fully matched" in {
+      Get("/pathEnd") ~> demo1Route ~> check {
+        responseAs[String]  === "/pathEnd"
+      }
+      Get("/pathEnd/continue") ~> demo1Route ~> check {
+        responseAs[String]  === "continue"
+      }
+      Get("/pathEnd/") ~> demo1Route ~> check {
+        handled === false
+      }
+    }
+
+    "return the path is gone and handle it" in {
+      Get("/handled/missing") ~> handledRoute ~> check {
+        responseAs[String] must contain("long gone")
+      }
+      Get("/handled/existing") ~> handledRoute ~> check {
+        handled === true
+        responseAs[String] === "This path exists"
+      }
+    }
+
   }
 
 }
